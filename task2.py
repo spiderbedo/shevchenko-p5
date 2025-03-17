e10c10ebb1b2da866fe0be6f499fd31b8047de72
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-17 14:07:14 +0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,23 @@
+import turtle as t
 
+xc = int(input())
+yc = int(input())
+r = int(input())
+x = int(input())
+y = int(input())
+
+if (xc - x) ^ 2 + (yc - y) ^ 2 < r ** 2:
+    print("Точка внутри окружности.")
+else:
+    print("Точка вне окружности.")
+
+t.up()
+t.setposition(xc, yc - r)
+t.down()
+t.circle(r)
+t.up()
+t.setposition(x, y)
+t.down()
+t.dot(10)
+t.done()
+print('\n')

2510eedf507c7fb98fa605c183c32af6067e5b04
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-17 14:10:10 +0700

    Update task7.py

--- task7.py
+++ task7.py
@@ -1,1 +1,14 @@
-
+numbers = list(map(int, input().split()))
+N = numbers[0]
+K = numbers[1]
+M = numbers[2]
+if K >= M:
+    if K - M - 1 >= (N - K) + (M - 1):
+        print((N - K) + (M - 1))
+    else:
+        print(K - M - 1)
+elif M > K:
+    if M - K - 1 >= (N - M) + (K - 1):
+        print((N - M) + (K - 1))
+    else:
+        print(M - K - 1)

5c559da5e7e614b5a16530cbd4aa72ea5ecf9a6e
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-17 14:08:58 +0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,1 +1,9 @@
-
+num = int(input())
+list = [2, 3, 4]
+list_2 = [0, 5, 6, 7, 8, 9]
+if num % 10 == 1:
+    print(num, "попугай")
+elif num % 10 in list:
+    print(num, "попугая")
+elif num % 10 in list_2:
+    print(num, "попугаев")

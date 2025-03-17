ba0f82f9bf3a3e622b8e0431652cce1e1576f39b
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-17 14:07:48 +0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,10 @@
-
+number = input()
+num = [i for i in number]
+count = 0
+for j in range(len(number) // 2):
+    if num[j] == num[-j-1]:
+        count += 1
+if count == len(number) // 2:
+    print("настоящее")
+else:
+    print("кривое")

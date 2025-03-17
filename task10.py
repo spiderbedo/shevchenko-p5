b513153d56baf03f9c86ea79f8b8f2f3fc9b5924
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2025-03-17 14:11:45 +0700

    Update task10.py

--- task10.py
+++ task10.py
@@ -1,1 +1,13 @@
-
+pincode = input()
+if len(pincode) == 4:
+    list_numbers = [i for i in pincode]
+    set_numbers = set(list_numbers)
+    if len(list_numbers) == len(set_numbers):
+        if int(pincode) in range(1900, 2051):
+            print("ERROR")
+        else:
+            print("OK")
+    else:
+        print("ERROR")
+else:
+    print("ERROR")
